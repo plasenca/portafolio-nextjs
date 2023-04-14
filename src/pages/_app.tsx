f4db0b5e1a3e0026e00ea919c4fb5dd6a8f7572a
Franzua Plasencia <franzua.15pc@gmail.com>
2023-04-14 01:50:33 -0500

    Configurate NextUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import { NextUIProvider } from '@nextui-org/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@/styles/globals.css'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(
+    <>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
+  );
 }
